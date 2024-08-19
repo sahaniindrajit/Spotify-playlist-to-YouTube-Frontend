791c57d73eb13c179c5433d88322afc694680bcd
Indrajit sahani <indrajit38mig@gmail.com>
2024-08-19 12:57:55 +0530

    backend link

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
                 </p>
 
                 <GoogleButton onClick={() => {
-                    window.location.href = 'http://localhost:3000/user/google/auth'; // Backend URL
+                    window.location.href = 'https://spotify-playlist-to-youtube-backend.onrender.com/user/google/auth'; // Backend URL
                 }} />
             </div>
         </section>
