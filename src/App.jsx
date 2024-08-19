379b4c2d953eda3009347ae832cb04fdd82c9ffb
Indrajit sahani <indrajit38mig@gmail.com>
2024-08-19 12:55:20 +0530

    production

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/playlist' element={<Playlist />} />
-          <Route path="*" element={<PageNotFound />} />
+          <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/user': 'http://localhost:3000'
-    }
-  },
   plugins: [react()],
 })
